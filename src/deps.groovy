2195818ba75a64404f8bb45c919f446924d81ffe
ivannikolaev <ivan.nikolaeff@yandex.ru>
2018-07-30 13:37:30 +0500

    added deps.groovy script

---
+++ src/deps.groovy
@@ -0,0 +1,34 @@
+import groovy.xml.MarkupBuilder
+
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+
+/**
+ * @author inikolaev
+ */
+def folder = new File(args[0]);
+def files = folder.listFiles({ File dir, String name -> name.toLowerCase().endsWith(".jar") } as FilenameFilter)
+def pomWriter = new FileWriter("dependencies.txt")
+def noPomWriter = new FileWriter("nopom.txt")
+def xml = new MarkupBuilder(pomWriter)
+files.each { jarFile ->
+    def zipFile = new ZipFile(jarFile)
+    xml.dependencies {
+        def pom = zipFile.entries().find {
+            !it.directory && it.name.toLowerCase().endsWith("/pom.properties")
+        } as ZipEntry
+        Properties props = new Properties()
+        props.load(zipFile.getInputStream(pom))
+        if (pom != null) {
+            dependency {
+                groupId props.getProperty("groupId")
+                artifactId props.getProperty("artifactId")
+                version props.getProperty("version")
+            }
+        } else {
+            noPomWriter.write(jarFile.name)
+        }
+    }
+}
+pomWriter.close()
+noPomWriter.close()
